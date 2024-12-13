e0d85552d2bd0dd8ae78c312255e7b25045ab073
Flint92 <1992flint@gmail.com>
2024-12-13 09:06:38 +0800

    test ping

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,10 @@
 mod resp;
 
-use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use crate::resp::{RespHandler, Value};
+use anyhow::Result;
+use bytes::BufMut;
+use tokio::io::AsyncReadExt;
+use tokio::net::{TcpListener, TcpStream};
 
 async fn handle_connection(stream: TcpStream) {
     let mut handler = RespHandler::new(stream);
@@ -10,15 +12,36 @@ async fn handle_connection(stream: TcpStream) {
     loop {
         let value = handler.read_value().await.unwrap();
 
-        let response = match value {
-            // TODO handle echo and ping
-            Value::SimpleString(_) => todo!(),
-            Value::BulkString(_) => todo!(),
-            Value::Array(_) => todo!(),
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap();
+            match command.as_str() {
+                "ping" => Value::SimpleString("PONG".into()),
+                "echo" => args.first().unwrap().clone(),
+                "byte" => break,
+                c => panic!("unknown command {}", c),
+            }
+        } else {
+            break;
         };
 
         handler.write_value(response).await.unwrap();
     }
+}
+
+fn extract_command(value: Value) -> Result<(String, Vec<Value>)> {
+   match value {
+       Value::Array(values) => {
+           Ok((
+               values.first().unwrap().serialize(),
+               values.into_iter().skip(1).collect(),
+           ))
+       },
+       Value::SimpleString(s) => Ok((s, vec![])),
+       Value::Integer(i) => Ok((String::new(), vec![])), // todo
+       Value::Error(e) => Ok((e, vec![])),
+       Value::BulkString(s) => Ok((s, vec![])),
+       _ => Err(anyhow::anyhow!("Unexpected command format")),
+   }
 }
 
 #[tokio::main]
--- server/src/resp.rs
+++ server/src/resp.rs
@@ -1,12 +1,19 @@
-use anyhow::Error;
-use bytes::BytesMut;
-use tokio::net::TcpStream;
+use std::io::BufRead;
 use anyhow::{anyhow, Result};
+use bytes::BytesMut;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
+const CR: u8 = b'\r';
+const LF: u8 = b'\n';
+
+#[derive(Clone)]
 pub enum Value {
     SimpleString(String),
     BulkString(String),
     Array(Vec<Value>),
+    Error(String),
+    Integer(i64),
 }
 
 impl Value {
@@ -18,7 +25,21 @@ impl Value {
             Value::BulkString(s) => {
                 format!("${}\r\n{}\r\n", s.chars().count(), s)
             },
-            _ => "-Error unsupported command\r\n".to_string()
+            Value::Array(values) => {
+                let mut s = format!("*{}\r\n", values.len());
+
+                for value in values.iter() {
+                    s += value.serialize().as_str();
+                }
+
+                s
+            },
+            Value::Error(s) => {
+                format!("-{}\r\n", s)
+            },
+            Value::Integer(i) => {
+                format!(":{}\r\n", i)
+            },
         }
     }
 }
@@ -36,14 +57,28 @@ impl RespHandler {
         }
     }
 
-    pub async fn read_value(&mut self) -> Result<Value> {
-        // TODO read from socket and parse
-        todo!()
+    pub async fn read_value(&mut self) -> Result<Option<Value>> {
+        let mut  bytes = [0u8; 512];
+
+        let bytes_read = self.stream.read(&mut bytes).await?;
+
+        if bytes_read == 0 {
+            return Ok(None);
+        }
+
+        self.buffer.extend_from_slice(&bytes[..bytes_read]);
+
+        let (v, _) = parse_message(self.buffer.split())?;
+
+        self.buffer.clear();
+
+        Ok(Some(v))
     }
 
     pub async fn write_value(&mut self, value: Value) -> Result<()> {
-        // TODO serialise and write to the socket
-        todo!()
+        self.stream.write(value.serialize().as_bytes()).await?;
+
+        Ok(())
     }
 
 }
@@ -52,17 +87,60 @@ fn parse_message(buf: BytesMut) -> Result<(Value, usize)> {
     match buf[0] as char {
         '+' => parse_simple_string(buf),
         '*' => parse_array(buf),
+        '-' => parse_error(buf),
+        ':' => parse_integer(buf),
         '$' => parse_bulk_string(buf),
-        _ => Err(anyhow!("Not a known value type {}", buf)),
+        _ => Err(anyhow!("Not a known value type {:?}", buf)),
     }
 }
 
-fn parse_bulk_string(p0: BytesMut) -> Result<(Value, usize)> {
-    todo!()
+fn parse_integer(buf: BytesMut) -> Result<(Value, usize)> {
+    if let Some((line, len)) = read_util_crlf(&buf[1..]) {
+        return Ok((Value::Integer(parse_int(line)?), len + 1))
+    }
+
+    Err(anyhow!("Not a error string {:?}", buf))
 }
 
-fn parse_array(p0: BytesMut) -> Result<(Value, usize)> {
-    todo!()
+fn parse_error(buf: BytesMut) -> Result<(Value, usize)> {
+    if let Some((line, len)) = read_util_crlf(&buf[1..]) {
+        let str = String::from_utf8(line.to_vec())?;
+        return Ok((Value::Error(str), len + 1))
+    }
+
+    Err(anyhow!("Not a error string {:?}", buf))
+}
+
+fn parse_bulk_string(buf: BytesMut) -> Result<(Value, usize)> {
+    let (bulk_str_len, bytes_consumed) = if let Some((line, len)) = read_util_crlf(&buf[1..]) {
+        let bulk_str_len = parse_int(line)?;
+        (bulk_str_len, len + 1)
+    } else {
+        return Err(anyhow!("Invalid bulk array format {:?}", buf));
+    };
+
+    let end_of_bulk_str = bytes_consumed + bulk_str_len as usize;
+    let total_parsed = end_of_bulk_str + 2;
+
+    Ok((Value::BulkString(String::from_utf8(buf[bytes_consumed..end_of_bulk_str].to_vec())?), total_parsed))
+}
+
+fn parse_array(buf: BytesMut) -> Result<(Value, usize)> {
+    let (array_len, mut bytes_consumed) = if let Some((line, len)) = read_util_crlf(&buf[1..]) {
+        let array_len = parse_int(line)?;
+        (array_len, len + 1)
+    } else {
+        return Err(anyhow!("Invalid array format {:?}", buf));
+    };
+
+    let mut itmes = vec![];
+    for _ in 0..array_len {
+        let (array_item, len) = parse_message(BytesMut::from(&buf[bytes_consumed..]))?;
+        itmes.push(array_item);
+        bytes_consumed += len;
+    }
+
+    Ok((Value::Array(itmes), bytes_consumed))
 }
 
 fn parse_simple_string(buf: BytesMut) -> Result<(Value, usize)> {
@@ -71,12 +149,16 @@ fn parse_simple_string(buf: BytesMut) -> Result<(Value, usize)> {
         return Ok((Value::SimpleString(str), len + 1))
     }
 
-    Err(anyhow!("Not a simple string {}", buf))
+    Err(anyhow!("Not a simple string {:?}", buf))
+}
+
+fn parse_int(buf: &[u8]) -> Result<i64> {
+    Ok(String::from_utf8(buf.to_vec())?.parse::<i64>()?)
 }
 
 fn read_util_crlf(buf: &[u8]) -> Option<(&[u8], usize)> {
     for i in 1..buf.len() {
-        if buf[i-1] == b'\r' && buf[i] == b'\n' {
+        if buf[i-1] == CR && buf[i] == LF {
             return Some((&buf[0..(i - 1)], i + 1))
         }
     }
